47fd95850a0d3cfa92e4a00546992af535c77a7f
tharushi lakshani <tharushilakshani81@gmail.com>
2025-05-06 21:34:16 +0530

    fix Navbar link for Products route

--- online-store/src/components/Navbar.jsx
+++ online-store/src/components/Navbar.jsx
@@ -15,7 +15,7 @@ function Navbar() {
             
             <ul className="nav-links">
                 <li><Link to="/">Home</Link></li>
-                <li><Link to="/products">Products</Link></li>
+                <li><Link to="/product">Products</Link></li>
                 <li><Link to="/about">About</Link></li>
                 <li><Link to="/contact">Contact</Link></li>
             </ul>
