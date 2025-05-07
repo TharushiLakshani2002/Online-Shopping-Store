dd9a4437fd6bac359f76f622603ce7852dc5fb9f
tharushi lakshani <tharushilakshani81@gmail.com>
2025-05-07 14:50:03 +0530

    import from App component

--- online-store/src/App.jsx
+++ online-store/src/App.jsx
@@ -1,6 +1,6 @@
 import { useState } from 'react';
 import Home from './pages/Home';
-import { Link } from 'react-router-dom';
+
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import './App.css';
 import About from './pages/About';
